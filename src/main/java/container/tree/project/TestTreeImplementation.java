0b72faf9f6e4cf2c0afd64f49b3d532c05974e43
notebook <krawmaciej@gmail.com>
2020-08-30 11:08:53 +0200

    Add TestTreeImplementation with package private fields/methods and its test (test tree implementation)

---
+++ src/main/java/container/tree/project/TestTreeImplementation.java
@@ -0,0 +1,188 @@
+package container.tree.project;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+
+class TestTreeImplementation<E extends Comparable<E>> implements Set<E> {
+	
+	static class Node<E> {
+		E value;
+		Node<E> left;
+		Node<E> right;
+		
+		Node(E value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	Node<E> root;
+	int size;
+	
+	TestTreeImplementation() {
+		this.root = null;
+		this.size = 0;
+	}
+
+	boolean addNode(E value, Node<E> node) {
+		if (value.compareTo(node.value) < 0) { // go left
+			if (node.left == null) {
+				node.left = new Node<E>(value);
+				return true;
+			} else {
+				return addNode(value, node.left);
+			}
+		} else if (value.compareTo(node.value) > 0) { // go right
+			if (node.right == null) {
+				node.right = new Node<E>(value);
+				return true;
+			} else {
+				return addNode(value, node.right);
+			}
+		} else { // is equal
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean add(E value) {
+		if(value == null) {
+			throw new NullPointerException("TreeSet cannot contain null entries.");
+		}
+		
+		boolean result = false;
+		if (root == null) {
+			root = new Node<E>(value);
+			result = true;
+		} else {
+			result = addNode(value, root);
+		}
+		
+		if (result) {
+			size++;
+		}
+		return result;
+	}
+
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		if(c == null) {
+			throw new NullPointerException("Collection may not have been initialized.");
+		}
+		
+		boolean result = false;
+		for (E value: c) {
+			result = this.add(value) ? true : result;
+		}
+		return result;
+	}
+
+
+	@Override
+	public void clear() {
+		size = 0;
+		// TODO Auto-generated method stub
+		
+	}
+
+	boolean containsNodeWith(E value, Node<E> node) {
+		if (node == null) // not contains
+			return false;
+		
+		if (value.compareTo(node.value) < 0) { // go left
+			return containsNodeWith(value, node.left);
+		} else if (value.compareTo(node.value) > 0) { // go right
+			return containsNodeWith(value, node.right);
+		} else { // is equal = contains
+			return true;
+		}
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (o == null) {
+			throw new NullPointerException("TreeSet cannot contain null entries.");
+		}
+		
+		if (root == null || !root.value.getClass().isInstance(o)) {
+			return false;
+		}
+		@SuppressWarnings("unchecked")
+		E value = (E) o;
+		return containsNodeWith(value, root);
+	}
+
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		if(c == null) {
+			throw new NullPointerException("Collection may not have been initialized.");
+		}
+		
+		boolean result = true;
+		for (Object value: c) {
+			result = result && this.contains(value);
+		}
+		return result;
+	}
+
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean remove(Object o) {
+		size--;
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/test/java/container/tree/project/TestTreeImplementationTest.java
@@ -0,0 +1,97 @@
+package container.tree.project;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Given a binary tree")
+class TestTreeImplementationTest {
+	
+	private TestTreeImplementation<Integer> myTree;
+	
+	@Nested
+	@DisplayName("When the tree is empty")
+	class EmptyTree {
+		
+		@BeforeEach
+		void setUp() {
+			myTree = new TestTreeImplementation<Integer>();
+		}
+		
+		@Test
+		@DisplayName("Then root is null") 
+		void testIfEmptyTree() {
+			assertAll("Veryfiy that tree is empty",
+					() -> assertNull(myTree.root),
+					() -> assertEquals(0, myTree.size)
+			);
+		}
+		
+		// checks if node is created correctly
+		@Test
+		@DisplayName("and a value was added Then root's children are null") 
+		void testIfEmptyRootChildren() {
+			assertTrue(myTree.add(2), () -> "Add root node");
+			TestTreeImplementation.Node<Integer> root = myTree.root;
+			
+			assertAll("Veryfiy that root node children are empty",
+					() -> assertNotNull(root),
+					() -> assertEquals(2, root.value),
+					() -> assertNull(root.left),
+					() -> assertNull(root.right)
+			);
+		}
+	}
+	
+	@Nested
+	@DisplayName("When the tree has four nodes")
+	class FourNodeTree {
+		/*
+		 * 		2
+		 * 	  0   3
+		 *     1
+		 */
+		@BeforeEach
+		void setUp() {
+			myTree = new TestTreeImplementation<Integer>();
+			myTree.addAll(Arrays.asList(2, 3, 0, 1));
+		}
+		
+		@Test
+		@DisplayName("Then the nodes should be in a correct order") 
+		void testAdd() {
+			TestTreeImplementation.Node<Integer> root = myTree.root;
+			TestTreeImplementation.Node<Integer> right3 = root.right;
+			TestTreeImplementation.Node<Integer> left0 = root.left;
+			TestTreeImplementation.Node<Integer> leftRight1 = left0.right;
+			assertAll("Veryfiy that the tree builds correctly",
+					// not null
+					() -> assertNotNull(root),
+					() -> assertNotNull(right3),
+					() -> assertNotNull(left0),
+					() -> assertNotNull(leftRight1),
+					// equal
+					() -> assertEquals(2, root.value),
+					() -> assertEquals(3, right3.value),
+					() -> assertEquals(0, left0.value),
+					() -> assertEquals(1, leftRight1.value),
+					// null
+					() -> assertNull(right3.left),
+					() -> assertNull(right3.right),
+					() -> assertNull(left0.left),
+					() -> assertNull(leftRight1.left),
+					() -> assertNull(leftRight1.right)
+			);
+		}
+	}
+}
